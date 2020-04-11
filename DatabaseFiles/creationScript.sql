ea2c1ee895dd04e1a2b28427fbe59048ac75828e
Miguel Ruano <miguelruano35@gmail.com>
2020-04-10 20:41:43 -0600

    V2.0.0 Cambios Finales

--- DatabaseFiles/creationScript.sql
+++ DatabaseFiles/creationScript.sql
@@ -12,3 +12,51 @@ CREATE TABLE IF NOT EXISTS person (
 );
 
 select * from person;
+DROP PROCEDURE actualizarSaldo;
+
+DELIMITER $$
+
+CREATE PROCEDURE actualizarSaldo (
+    IN  porcentaje INT
+)
+BEGIN
+    DECLARE finished INTEGER DEFAULT 0;
+    DECLARE person_id_temp INTEGER DEFAULT 0;
+    
+       -- declare cursor for people
+    DEClARE curPeople 
+        CURSOR FOR 
+            SELECT person_id FROM person;
+ 
+    -- declare NOT FOUND handler
+    DECLARE CONTINUE HANDLER 
+        FOR NOT FOUND SET finished = 1;
+        
+        
+	OPEN curPeople;
+ 
+    updateBalances: LOOP
+        FETCH curPeople INTO person_id_temp;
+
+        IF finished = 1 THEN 
+            LEAVE updateBalances;
+        END IF;
+        -- build email list
+        UPDATE person
+			SET person_balance = (person_balance + 5) * (porcentaje / 100)
+		WHERE person_id = person_id_temp
+        ;
+    END LOOP updateBalances;
+    CLOSE curPeople;
+    
+	
+    /* UPDATE person
+    SET person_balance = (person_balance + 5) * (porcentaje / 100)
+    WHERE person_id = personId;
+    */
+END$$
+ 
+DELIMITER ;
+
+
+CALL actualizarSaldo(10);
